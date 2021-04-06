df7f0be569f442b3c552612b4c9711b8501fa29c
Tony Ketcham <tonyketcham@gmail.com>
2021-04-06 16:25:03 -0500

    🏍 calm down, eslint...

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
 		content: ['./src/**/*.{html,js,svelte,ts}'],
 		options: {
 			defaultExtractor: (content) => [
+				/* eslint-disable no-unused-vars */
 				// If this stops working, please open an issue at https://github.com/svelte-add/tailwindcss/issues rather than bothering Tailwind Labs about it
 				...tailwindExtractor(content),
 				// Match Svelte class: directives (https://github.com/tailwindlabs/tailwindcss/discussions/1731)
