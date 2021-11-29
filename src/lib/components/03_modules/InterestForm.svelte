d3da1950486ddadb4f795028e4c4015ba1fe7041
Tony Ketcham <tonyketcham@gmail.com>
2021-11-29 03:37:24 -0600

    style(contact): interest form UX + responsive design

--- src/lib/components/03_modules/InterestForm.svelte
+++ src/lib/components/03_modules/InterestForm.svelte
@@ -43,6 +43,14 @@
 	}
 
 	/**
+	 * Go back to the previous step in the form.
+	 */
+	function prevStep() {
+		if (currentStepIndex === 0) return;
+		currentStepIndex--;
+	}
+
+	/**
 	 * POST request to email data via serverless function
 	 */
 	async function sendMail() {
@@ -65,32 +73,44 @@
 
 <section
 	id="interested"
-	class="flex flex-row font-mono my-16 border-2 border-rich-black divide-x-2 divide-rich-black mx-8"
+	class="flex flex-col md:flex-row font-mono my-16 border-2 border-rich-black divide-y-2 md:divide-y-0 md:divide-x-2 divide-rich-black mx-8"
 >
-	<div class="grid place-content-center w-7/12 bg-sunglow p-32">
-		<h2 class="text-left tracking-tighter leading-[6rem] text-9xl font-bold font-mono">
+	<div class="grid place-content-center md:w-1/2 lg:w-7/12 bg-sunglow p-32">
+		<h2
+			class="text-left tracking-tighter xl:leading-[6rem] text-6xl lg:text-7xl xl:text-9xl font-bold font-mono"
+		>
 			Let's make something neat together
 		</h2>
 	</div>
-	<form class="space-y-4 w-5/12 p-16 grid place-content-center" on:submit|preventDefault={nextStep}>
+	<form
+		class="space-y-4 md:w-1/2 lg:w-5/12 p-16 grid place-content-center"
+		on:submit|preventDefault={nextStep}
+	>
 		{#if currentStep === 'contact info'}
 			<div class="text-input">
 				<label for="name">Name:</label>
-				<input type="text" name="name" id="name" autocomplete="on" required bind:value={name} />
+				<input type="text" name="name" id="name" autocomplete="name" required bind:value={name} />
 			</div>
 			<div class="text-input">
 				<label for="email">Email:</label>
-				<input type="email" name="email" id="email" autocomplete="on" required bind:value={email} />
+				<input
+					type="email"
+					name="email"
+					id="email"
+					autocomplete="email"
+					required
+					bind:value={email}
+				/>
 			</div>
 			<div class="text-input">
 				<label for="phone">Phone:</label>
-				<input type="tel" name="phone" id="phone" autocomplete="on" bind:value={phone} />
+				<input type="tel" name="phone" id="phone" autocomplete="tel" bind:value={phone} />
 			</div>
 			<div>
 				<h3>I'm interested in...</h3>
-				<div class="space-x-3">
-					{#each menu as service}
-						<label class="select-none">
+				{#each menu as service}
+					<div class="inline-block mr-6 mt-1">
+						<label class="select-none checkbox-input !text-xl">
 							<input
 								type="checkbox"
 								bind:group={selectedServices}
@@ -99,10 +119,28 @@
 							/>
 							{service}
 						</label>
-					{/each}
-				</div>
+					</div>
+				{/each}
 			</div>
 		{:else if currentStep === 'project details'}
+			<button
+				aria-label="go back"
+				class="transform hover:scale-110 transition-transform max-w-max"
+				on:click={prevStep}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="ai ai-ArrowLeft"><path d="M11 5l-7 7 7 7" /><path d="M4 12h16" /></svg
+				>
+			</button>
 			<div class="text-input">
 				<label for="details">What do you have in mind?</label>
 				<textarea name="details" id="details" rows="5" bind:value={details} />
@@ -141,5 +179,46 @@
 		@apply px-2 py-1 text-2xl;
 		@apply border-2 border-rich-black bg-white;
 		@apply font-semibold tracking-wide;
+	}
+	input,
+	textarea,
+	button {
+		@apply outline-none;
+	}
+	/* Have to annoyingly do this because of 'Semicolon or block is expected  ParseError' bug with linters, Tailwind, and SvelteKit battle royale */
+	input:focus,
+	textarea:focus,
+	button:focus {
+		@apply outline-black;
+	}
+	.checkbox-input {
+		@apply flex;
+	}
+	input[type='checkbox'] {
+		transform: translateY(-0.075em);
+
+		@apply appearance-none m-auto mr-4;
+		@apply grid place-content-center;
+		@apply w-4 h-4 text-current bg-platinum;
+		@apply border-2 border-current;
+	}
+
+	input[type='checkbox']::before {
+		content: '';
+		@apply w-2 h-2;
+		@apply outline-none;
+		transform: scale(0);
+		transition: 50ms transform ease-in-out;
+
+		/* Windows High Contrast Mode */
+		background-color: CanvasText;
+	}
+
+	input[type='checkbox']:checked::before {
+		transform: scale(1);
+	}
+
+	input[type='checkbox']:focus {
+		@apply border-blue-400 outline-none;
 	}
 </style>
